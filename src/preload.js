1f2dbbc681d09d774252d247ed4e3a770cbdd47c
tomek66666666665 <154760997+tomek66666666665@users.noreply.github.com>
2026-09-14 18:22:00 +0200

    Delete src directory

--- src/preload.js
+++
@@ -1,9 +0,0 @@
-const {contextBridge,ipcRenderer}=require("electron");
-contextBridge.exposeInMainWorld("moon",{
-  navigate:u=>ipcRenderer.invoke("navigate",u),
-  home:()=>ipcRenderer.invoke("home"),
-  back:()=>ipcRenderer.invoke("back"),
-  forward:()=>ipcRenderer.invoke("forward"),
-  chooseVPN:()=>ipcRenderer.invoke("choose-vpn"),
-  loadExtension:()=>ipcRenderer.invoke("load-extension")
-});
